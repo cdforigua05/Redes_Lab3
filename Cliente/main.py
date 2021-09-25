7b055b987daa1379e679ea688f0d895ec48f8ba7
MateoCh <53842157+MateoCh@users.noreply.github.com>
2021-09-25 17:16:49 -0500

    logs funcionanding anding

--- Cliente/main.py
+++ Cliente/main.py
@@ -1,51 +1,107 @@
 import hashlib
+from os import set_inheritable
 import socket
+from socket import error
 import argparse
 from threading import Thread
-
-SIZE = 1024
-cn=25
+from datetime import date, datetime
+import os
+import threading
+import time
+SIZE = 2048
+cn=5
 FORMAT = "utf-8"
 parser = argparse.ArgumentParser()
 parser.add_argument("--threads", type=int, default=cn, help="Number of clients")
+parser.add_argument("--file_id",type=int, choices=[1,2], default=1, help="1 for 100MB file, 2 for 250 MB file")
 
 client_connected = 0
 
 class Client(Thread):
-    def __init__(self, i):
+    def __init__(self, i, logger, lock):
         Thread.__init__(self)
         self.i = i
+        self.logger = logger
+        self.lock = lock
     def run(self):
         print("Cliente "+str(self.i)+" iniciando conexión")
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect(("localhost", 9879))
-        print("Cliente"+str(self.i)+ "conectado")
+        print("Cliente "+str(self.i)+ " conectado")
         
         s.send('Start transmision OK'.encode(FORMAT))
-        filename = "ArchivosRecibidos/Cliente"+str(self.i)+"-Prueba-"+str(cn)
-
+        filename = "ArchivosRecibidos/Cliente"+str(self.i)+"-Prueba-"+str(args.threads)
+        sizefile = s.recv(SIZE).decode(FORMAT)
         dataHash = s.recv(SIZE).decode(FORMAT)
         print(str(self.i)+"hash:"+dataHash)
-        data = s.recv(SIZE).decode(FORMAT)
-        dataEn= data.encode(FORMAT)
-        print(str(self.i)+"Data:"+data)
-        vhash = hashlib.md5(dataEn).hexdigest()
+        data = ""
+        i=0
+        time_inicio = time.time()
+        paquetes = 0
+        bytes_enviados = 0
+        while True:
+            try:
+                input_data = s.recv(SIZE).decode(FORMAT)
+                paquetes+=1
+            except error:
+                print ("Error de lectura")
+                break
+            else:
+                if input_data:
+                    # Compatibilidad con Python 3.
+                    if input_data.endswith("Termino:200"):
+                        data+=input_data.replace("Termino:200","")
+                        bytes_enviados+=len(input_data.replace("Termino:200",""))
+                        break
+                    else:
+                        # Almacenar datos.
+                        data+=input_data
+                        bytes_enviados+=len(input_data)
+                        
+        print("Archivo recibido por completo en el cliente", self.i)
+        time_final = time.time()
+        contenido_output = ""
+        contenido_output += "Tamanio archivo: "+str(sizefile)+"\n"
+        contenido_output +="Tiempo de transferencia"+str(self.i)+" es "+ str(time_final-time_inicio)+"\n"
+        contenido_output += "Paquetes recibidos por el cliente "+str(self.i)+" son "+ str(paquetes)+"\n"
+        contenido_output += "Bytes recibidos por el cliente "+str(self.i)+" son "+ str(bytes_enviados)+"\n"
+        #data = s.recv(SIZE).decode(FORMAT)
+        #print(str(self.i)+"Data:"+data)
+        data= data.encode(FORMAT)
+        vhash = hashlib.md5(data).hexdigest()
         print(str(self.i)+"vhash:"+vhash)
+        print(str(self.i)+"dataHash:"+dataHash)
         if vhash==dataHash:
-            file = open(filename, "w")
+            file = open(filename, "wb")
             file.write(data)
             file.close()
+            print("hash correcto cliente "+str(self.i))
+            contenido_output += "Archivo recibido correctamente por cliente "+str(self.i)+"\n"
+            s.send(("hash correcto cliente "+str(self.i)).encode(FORMAT))
         else:
-            print("hash incorrecto")
-            s.send("hash incorrecto".encode(FORMAT))        
-
+            print("hash incorrecto cliente "+str(self.i))
+            contenido_output +="Archivo NO recibido correctamente por cliente"+str(self.i)+"\n"
+            s.send(("hash correcto cliente "+str(self.i)).encode(FORMAT))        
+        contenido_output+="\n"
+        self.lock.acquire()
+        self.logger.write(contenido_output)
+        self.lock.release()
         s.close()
 
 
 
 def main(args):
-    for i in range(cn):
-        c= Client(i)
+    fecha = datetime.now()
+    date_time = fecha.strftime("%m-%d-%Y-%H-%M-%S")
+    log = open("logs/"+date_time+"-log.txt", "w")
+    if args.file_id==1:
+        file_name = "100.txt"
+    else:
+        file_name = "250.txt"
+    log.write("Nombre archivo: "+ file_name+"\n")
+    for i in range(args.threads):
+        lock = threading.Lock()
+        c=Client(i,log, lock)
         c.start()      
         
 
--- Server/main.py
+++ Server/main.py
@@ -1,40 +1,81 @@
 from abc import abstractmethod
 import socket
-from threading import Thread
+from threading import Lock, Thread
 import hashlib
 import pickle
 import logging
 from datetime import date, datetime
 import argparse
-
-SIZE = 1024
+import os
+import time
+import threading
+SIZE = 2048
 FORMAT = "utf-8"
-cn=25
+FILE_END = "FILE_END"
+cn=5
 parser = argparse.ArgumentParser()
-parser.add_argument("--file",type=int, choices=[1,2], help="1 for 100MB file, 2 for 250 MB file")
+parser.add_argument("--file_id",type=int, choices=[1,2], default=1, help="1 for 100MB file, 2 for 250 MB file")
 parser.add_argument("--threads", type=int, default=cn, help= "Number of clients")
 
 class Server(Thread):
-    def __init__(self, conn, addr, logger):
+    def __init__(self, conn, addr, logger, file_id, lock, sizefile):
         Thread.__init__(self)
         self.conn = conn
         self.addr = addr
         self.logger=logger
+        self.file_id = file_id
+        self.lock = lock
+        self.sizefile = sizefile
     def run(self):
         input_data=self.conn.recv(SIZE).decode(FORMAT)
         print(input_data)
-        if input_data==1:
-            file_name = "./data/100.txt"
+        if self.file_id==1:
+            file_name = "../Data/10.txt"
         else:
-            file_name = "./data/250.txt"
+            file_name = "../Data/10.txt"
         file = open(file_name, "r")
         data = file.read()
         dataEn=data.encode(FORMAT)
         dataHash = hashlib.md5(dataEn).hexdigest()
+        sf = str(self.sizefile)
+        self.conn.send(sf.encode(FORMAT))
         self.conn.send(dataHash.encode(FORMAT))
-        self.conn.send(dataEn)
+        print("Hash:",dataHash)
+        time_inicio = time.time()
+        paquetes = 0
+        bytes_enviados =0
+        while True:
+            file = open(file_name, "rb")
+            content_file = file.read(SIZE)
+            while content_file:
+                self.conn.send(content_file)
+                bytes_enviados += len(content_file)
+                content_file = file.read(SIZE)
+                paquetes+=1
+            break
+        print("Archivo enviado correctamente")
+        self.conn.send("Termino:200".encode(FORMAT))
+        recibido = self.conn.recv(SIZE).decode(FORMAT)
+        time_final = time.time()
+        contenido_output = ""
+        contenido_output +="Tiempo de transferencia para cliente "+str(self.addr)+" es "+ str(time_final-time_inicio)+"\n"
+        contenido_output += "Paquetes enviado al cliente "+str(self.addr)+" son "+ str(paquetes)+"\n"
+        contenido_output += "Bytes enviados al cliente "+str(self.addr)+" son "+ str(bytes_enviados)+"\n"
+
+        print(recibido)
+        if not "incorrecto" in recibido:
+            contenido_output += "Transferencia exitosa para cliente "+str(self.addr)+"\n"
+        else:
+            contenido_output +="Transferencia NO exitosa para cliente "+str(self.addr)+"\n"
+        contenido_output+="\n"
+        self.lock.acquire()
+        self.logger.write(contenido_output)
+        self.lock.release()
+        # self.conn.send(dataEn)
         file.close()
         self.conn.close()
+
+        
 
 def main(args):
     s =socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -42,9 +83,14 @@ def main(args):
     s.listen(cn)
     fecha = datetime.now()
     date_time = fecha.strftime("%m-%d-%Y-%H-%M-%S")
-    logger = logging.getLogger(date_time+"")
-    logger.setLevel(logging.INFO)
-    logger.info("Prueba"+date_time)
+    log = open("logs/"+date_time+"-log.txt", "w")
+    if args.file_id==1:
+        file_name = "../Data/100.txt"
+    else:
+        file_name = "../Data/250.txt"
+    log.write("Nombre archivo: "+ file_name+"\n")
+    sizefile = os.stat(file_name).st_size
+    log.write("Tamanio archivo: "+str(sizefile)+"\n")
     connections = []
     while len(connections)!=args.threads:
         conn, addr = s.accept()
@@ -53,9 +99,13 @@ def main(args):
     for con in connections:
         conn=con[0]
         addr=con[1]
-        c = Server(conn, addr, logger)
+        lock = threading.Lock()
+        c = Server(conn, addr, log, args.file_id, lock, sizefile)
         c.start()
         print("Se ha conectado a", addr)
+        log.write("Se ha conectado cliente "+str(addr)+"\n")
+
+    
 
 if __name__ == "__main__":
     args = parser.parse_args()
