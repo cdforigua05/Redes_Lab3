58cbb2151ce5a456be045082b3e870379ad3be71
MateoCh <53842157+MateoCh@users.noreply.github.com>
2021-09-24 23:31:20 -0500

    funcionanding anding

--- Cliente/main.py
+++ Cliente/main.py
@@ -1,35 +1,36 @@
 import hashlib
 import socket
 import argparse
-from concurrent.futures import ThreadPoolExecutor
+from threading import Thread
 
 SIZE = 1024
-cn=1
+cn=25
 FORMAT = "utf-8"
 parser = argparse.ArgumentParser()
 parser.add_argument("--threads", type=int, default=cn, help="Number of clients")
 
 client_connected = 0
 
-
-def super_task(args, i):
-    print("Cliente "+str(i)+" iniciando conexión")
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect(("localhost", 8000))
-    print("Cliente"+str(i)+ "conectado")
-    global client_connected
-    client_connected += 1
-    if client_connected == args.threads:
+class Client(Thread):
+    def __init__(self, i):
+        Thread.__init__(self)
+        self.i = i
+    def run(self):
+        print("Cliente "+str(self.i)+" iniciando conexión")
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect(("localhost", 9879))
+        print("Cliente"+str(self.i)+ "conectado")
+        
         s.send('Start transmision OK'.encode(FORMAT))
-        filename = "Cliente"+str(i)+"-Prueba-"+str(cn)
+        filename = "ArchivosRecibidos/Cliente"+str(self.i)+"-Prueba-"+str(cn)
 
         dataHash = s.recv(SIZE).decode(FORMAT)
-        print(str(i)+"hash:"+dataHash)
+        print(str(self.i)+"hash:"+dataHash)
         data = s.recv(SIZE).decode(FORMAT)
         dataEn= data.encode(FORMAT)
-        print(str(i)+"Data:"+data)
+        print(str(self.i)+"Data:"+data)
         vhash = hashlib.md5(dataEn).hexdigest()
-        print(str(i)+"vhash:"+vhash)
+        print(str(self.i)+"vhash:"+vhash)
         if vhash==dataHash:
             file = open(filename, "w")
             file.write(data)
@@ -43,10 +44,9 @@ def super_task(args, i):
 
 
 def main(args):
-    executor = ThreadPoolExecutor(max_workers=args.threads)
-    for i in range(args.threads):
-        executor.submit(super_task,args, i)
-        
+    for i in range(cn):
+        c= Client(i)
+        c.start()      
         
 
 if __name__ == "__main__":
--- Server/main.py
+++ Server/main.py
@@ -21,38 +21,38 @@ class Server(Thread):
         self.addr = addr
         self.logger=logger
     def run(self):
-        hasher = hashlib.md5()
-        while True:
-            input_data=self.conn.recv(SIZE).decode(FORMAT)
-            print(input_data)
-            if input_data==1:
-                file_name = "./data/100.txt"
-            else:
-                file_name = "./data/250.txt"
-            file = open(file_name, "r")
-            data = file.read()
-            dataEn=data.encode(FORMAT)
-            dataHash = hashlib.md5(dataEn).hexdigest()
-            self.conn.send(dataHash.encode(FORMAT))
-            self.conn.send(dataEn)
-            file.close()
-            #self.conn.close()
+        input_data=self.conn.recv(SIZE).decode(FORMAT)
+        print(input_data)
+        if input_data==1:
+            file_name = "./data/100.txt"
+        else:
+            file_name = "./data/250.txt"
+        file = open(file_name, "r")
+        data = file.read()
+        dataEn=data.encode(FORMAT)
+        dataHash = hashlib.md5(dataEn).hexdigest()
+        self.conn.send(dataHash.encode(FORMAT))
+        self.conn.send(dataEn)
+        file.close()
+        self.conn.close()
 
 def main(args):
     s =socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind(("localhost",8000))
+    s.bind(("localhost",9879))
     s.listen(cn)
     fecha = datetime.now()
     date_time = fecha.strftime("%m-%d-%Y-%H-%M-%S")
     logger = logging.getLogger(date_time+"")
     logger.setLevel(logging.INFO)
     logger.info("Prueba"+date_time)
-    #TODO Revisar conexiones
     connections = []
     while len(connections)!=args.threads:
         conn, addr = s.accept()
         print("Conectado con", addr)
         connections.append((conn, addr))
+    for con in connections:
+        conn=con[0]
+        addr=con[1]
         c = Server(conn, addr, logger)
         c.start()
         print("Se ha conectado a", addr)
