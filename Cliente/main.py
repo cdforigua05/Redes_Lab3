a256c32613992826c9c49581f0f786dd158634d6
MateoCh <53842157+MateoCh@users.noreply.github.com>
2021-09-24 22:44:53 -0500

    envia 1 archivo

--- Cliente/main.py
+++ Cliente/main.py
@@ -1,21 +1,44 @@
-from socket import socket
+import hashlib
+import socket
 import argparse
 from concurrent.futures import ThreadPoolExecutor
 
+SIZE = 1024
+cn=1
+FORMAT = "utf-8"
 parser = argparse.ArgumentParser()
-parser.add_argument("--threads", type=int, default=5, help="Number of clients")
+parser.add_argument("--threads", type=int, default=cn, help="Number of clients")
 
 client_connected = 0
 
+
 def super_task(args, i):
     print("Cliente "+str(i)+" iniciando conexión")
-    s = socket()
-    s.connect(("192.168.0.12", 8000))
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(("localhost", 8000))
     print("Cliente"+str(i)+ "conectado")
     global client_connected
     client_connected += 1
     if client_connected == args.threads:
-        s.sendall(b'Start transmision OK')
+        s.send('Start transmision OK'.encode(FORMAT))
+        filename = "Cliente"+str(i)+"-Prueba-"+str(cn)
+
+        dataHash = s.recv(SIZE).decode(FORMAT)
+        print(str(i)+"hash:"+dataHash)
+        data = s.recv(SIZE).decode(FORMAT)
+        dataEn= data.encode(FORMAT)
+        print(str(i)+"Data:"+data)
+        vhash = hashlib.md5(dataEn).hexdigest()
+        print(str(i)+"vhash:"+vhash)
+        if vhash==dataHash:
+            file = open(filename, "w")
+            file.write(data)
+            file.close()
+        else:
+            print("hash incorrecto")
+            s.send("hash incorrecto".encode(FORMAT))        
+
+        s.close()
 
 
 
--- Server/main.py
+++ Server/main.py
@@ -1,5 +1,5 @@
 from abc import abstractmethod
-from socket import socket, error
+import socket
 from threading import Thread
 import hashlib
 import pickle
@@ -7,45 +7,41 @@ import logging
 from datetime import date, datetime
 import argparse
 
+SIZE = 1024
+FORMAT = "utf-8"
+cn=25
 parser = argparse.ArgumentParser()
 parser.add_argument("--file",type=int, choices=[1,2], help="1 for 100MB file, 2 for 250 MB file")
-parser.add_argument("--threads", type=int, help= "Number of clients")
-
+parser.add_argument("--threads", type=int, default=cn, help= "Number of clients")
 
 class Server(Thread):
     def __init__(self, conn, addr, logger):
         Thread.__init__(self)
         self.conn = conn
         self.addr = addr
-        self.logger
+        self.logger=logger
     def run(self):
         hasher = hashlib.md5()
         while True:
-            try:
-                input_data = self.conn.recv(1024)
-            except error:
-                print("Error de lectura")
-                break
+            input_data=self.conn.recv(SIZE).decode(FORMAT)
+            print(input_data)
+            if input_data==1:
+                file_name = "./data/100.txt"
             else:
-                if input_data==1:
-                    file_name = "./data/100.txt"
-                else:
-                    file_name = "./data/250.txt"
-                with open(file_name, "rb") as f:
-                    buf = f.read()
-                    hasher.update(buf)
-                    codigo_hash = hasher.hexdigest()
-                    self.conn.send("Hash:"+ str(codigo_hash))
-                    #content = f.read(1024)
-                    #while content:
-                    #    self.conn.send(content)
-                    #    content = f.read(1024)
+                file_name = "./data/250.txt"
+            file = open(file_name, "r")
+            data = file.read()
+            dataEn=data.encode(FORMAT)
+            dataHash = hashlib.md5(dataEn).hexdigest()
+            self.conn.send(dataHash.encode(FORMAT))
+            self.conn.send(dataEn)
+            file.close()
+            #self.conn.close()
 
-                break
 def main(args):
-    s =socket()
+    s =socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind(("localhost",8000))
-    s.listen(25)
+    s.listen(cn)
     fecha = datetime.now()
     date_time = fecha.strftime("%m-%d-%Y-%H-%M-%S")
     logger = logging.getLogger(date_time+"")
@@ -57,9 +53,9 @@ def main(args):
         conn, addr = s.accept()
         print("Conectado con", addr)
         connections.append((conn, addr))
-        #c = Server(conn, addr, logger)
-        #c.start()
-        #print("Se ha conectado a", addr)
+        c = Server(conn, addr, logger)
+        c.start()
+        print("Se ha conectado a", addr)
 
 if __name__ == "__main__":
     args = parser.parse_args()
